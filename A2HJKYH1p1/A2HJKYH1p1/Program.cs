697217ec911cbb61573d3acbe24e66c71b4bb6e1
Hyunjin Kim <69027218+jennn613@users.noreply.github.com>
2021-10-02 13:56:37 +0900

    step one
    set variables and ask basic questions

---
+++ A2HJKYH1p1/A2HJKYH1p1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2HJKYH1p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string quarter; // yearly quarter that the customer registered
+            string membershipNum; // membership number
+            int itemNum; // number of items purchased
+            double itemCost; // cost of a single item
+            double totalCost; // total cost
+
+            Console.WriteLine("Please enter the qurater you regieterd in ");
+            quarter = Console.ReadLine();
+            Console.WriteLine("Enter your membership number: ");
+            membershipNum = Console.ReadLine();
+            Console.WriteLine("How many items did you buy?: ");
+            itemNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How much is single cost?: ");
+            itemCost = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
