5d0fbe40bf1e4cd710c0dd29beabe2c993385930
Hyunjin Kim <69027218+jennn613@users.noreply.github.com>
2021-10-02 15:09:29 +0900

    step two
    if statement

--- A2HJKYH1p1/A2HJKYH1p1/Program.cs
+++ A2HJKYH1p1/A2HJKYH1p1/Program.cs
@@ -1,34 +1,4 @@
-﻿/*
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace A2HJKYH1p1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string quarter; // yearly quarter that the customer registered
-            string membershipNum; // membership number
-            int itemNum; // number of items purchased
-            double itemCost; // cost of a single item
-            double totalCost; // total cost
-
-            Console.WriteLine("Please enter the qurater you regieterd in ");
-            quarter = Console.ReadLine();
-            Console.WriteLine("Enter your membership number: ");
-            membershipNum = Console.ReadLine();
-            Console.WriteLine("How many items did you buy?: ");
-            itemNum = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("How much is single cost?: ");
-            itemCost = Convert.ToInt32(Console.ReadLine());
-        }
-    }
-}*/
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -41,13 +11,13 @@ namespace A2HJKYHS1P1
         static void Main(string[] args)
         {
             // Defining variables
-            string quarter;
-            string membershipNum;
-            int itemNum;
-            double itemCost;
-            double totalCost;
-            double tax;
-            double finalCost;
+            string quarter; // yearly quarter that the customer registered
+            string membershipNum; // membership number
+            int itemNum; // number of items purchased
+            double itemCost; // cost of a single item
+            double totalCost; // total cost
+            double tax; // calculate tax
+            double finalCost; // final cost
 
             // Getting user input
             Console.WriteLine("Please enter the quarter you registered in: ");
@@ -67,13 +37,28 @@ namespace A2HJKYHS1P1
             tax = 0.13 * totalCost;
             finalCost = totalCost + tax;
 
+            if (5 < itemNum && itemNum < 10)
+            {
+                totalCost = totalCost - 20;
+            }
+            else if(10 < itemNum)
+            {
+                totalCost = totalCost * 70 / 100;
+            }
+            else
+            {
+                totalCost = totalCost * 87 / 100;
+            }
+            
+
+
             // Displaying information to the user
-            Console.WriteLine("");
-            Console.WriteLine(quarter + " is the quarter you registered in.");
-            Console.WriteLine("Membership number: " + membershipNum);
-            Console.WriteLine("Total items: " + itemNum);
-            Console.WriteLine("Total cost before adjustment: " + totalCost);
-            Console.WriteLine("Final cost: " + finalCost);
+            //Console.WriteLine("");
+            //Console.WriteLine(quarter + " is the quarter you registered in.");
+            //Console.WriteLine("Membership number: " + membershipNum);
+            //Console.WriteLine("Total items: " + itemNum);
+            //Console.WriteLine("Total cost before adjustment: " + totalCost);
+            //Console.WriteLine("Final cost: " + finalCost);
 
         }
     }
