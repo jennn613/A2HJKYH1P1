d18cdefce9fd91e750d4532db0d48b95e2997bb4
Cloudiiiii <Yusufhafeji33@gmail.com>
2021-10-02 14:20:50 -0400

    Update Program.cs
    try to run it and let me know how it works

--- A2HJKYH1p1/A2HJKYH1p1/Program.cs
+++ A2HJKYH1p1/A2HJKYH1p1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/*
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,6 +25,56 @@ namespace A2HJKYH1p1
             itemNum = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("How much is single cost?: ");
             itemCost = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2HJKYHS1P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Defining variables
+            string quarter;
+            string membershipNum;
+            int itemNum;
+            double itemCost;
+            double totalCost;
+            double tax;
+            double finalCost;
+
+            // Getting user input
+            Console.WriteLine("Please enter the quarter you registered in: ");
+            quarter = Console.ReadLine();
+
+            Console.WriteLine("Please enter your membership number: ");
+            membershipNum = Console.ReadLine();
+
+            Console.WriteLine("Please enter the number of items you are purchasing today: ");
+            itemNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the cost of the item you are purchasing today: ");
+            itemCost = Convert.ToDouble(Console.ReadLine());
+
+            // Calculations
+            totalCost = itemCost * itemNum;
+            tax = 0.13 * totalCost;
+            finalCost = totalCost + tax;
+
+            // Displaying information to the user
+            Console.WriteLine("");
+            Console.WriteLine(quarter + " is the quarter you registered in.");
+            Console.WriteLine("Membership number: " + membershipNum);
+            Console.WriteLine("Total items: " + itemNum);
+            Console.WriteLine("Total cost before adjustment: " + totalCost);
+            Console.WriteLine("Final cost: " + finalCost);
+
         }
     }
 }
