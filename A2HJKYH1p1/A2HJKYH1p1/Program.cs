e688ad82b600d008151f3c0691ece00c0374e491
Cloudiiiii <Yusufhafeji33@gmail.com>
2021-10-03 12:28:26 -0400

    Commented out old program, added a different one
    IKZ!

--- A2HJKYH1p1/A2HJKYH1p1/Program.cs
+++ A2HJKYH1p1/A2HJKYH1p1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,7 +24,7 @@ namespace A2HJKYHS1P1
             /* --------------------------------------------------------------------------- */
 
             // Getting user input
-            Console.WriteLine("Please enter the quarter you registered in: ");
+            /*Console.WriteLine("Please enter the quarter you registered in: ");
             quarter = Console.ReadLine();
 
             Console.WriteLine("Please enter your membership number: ");
@@ -91,6 +91,136 @@ namespace A2HJKYHS1P1
         }
 
         static double Discount(double a) // discount by first discounted cost, a = itemNum, b = totalCost
+        {
+            double furtherDiscountedTotalCost;
+
+            if (a > 100)
+            {
+                furtherDiscountedTotalCost = a * 97.5 / 100;
+                return furtherDiscountedTotalCost;
+            }
+            else if (a < 30)
+            {
+                furtherDiscountedTotalCost = a - 2;
+                return furtherDiscountedTotalCost;
+            }
+            else
+            {
+                return a;
+            }
+        }
+    }
+}
+*/
+using System;
+
+namespace A2HJKYHS1P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Defining variables
+            string quarter;
+            string membershipNum;
+            int itemNum;
+            double itemCost;
+            double totalCost;
+            double discountedTotalCost;
+            double furtherDiscountedTotalCost;
+            double tax;
+            double finalCost;
+
+            // Getting user input
+            Console.WriteLine("Please enter the quarter you registered in: ");
+            quarter = Console.ReadLine();
+
+            Console.WriteLine("Please enter your membership number: ");
+            membershipNum = Console.ReadLine();
+
+            Console.WriteLine("Please enter the number of items you are purchasing today: ");
+            itemNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the cost of the item you are purchasing today: ");
+            itemCost = Convert.ToDouble(Console.ReadLine());
+
+            // Calculations
+            totalCost = itemCost * itemNum;
+            discountedTotalCost = Discount(itemNum, totalCost);
+            furtherDiscountedTotalCost = Discount(discountedTotalCost);
+            tax = 0.13 * totalCost;
+            finalCost = totalCost + tax;
+
+            if (discountedTotalCost < 0)
+            {
+                discountedTotalCost = -(totalCost * 0.13);
+            }
+            if (furtherDiscountedTotalCost < 0)
+            {
+                furtherDiscountedTotalCost = 0;
+            }
+
+            // Displaying information to the user
+            Console.WriteLine("");
+            Console.WriteLine(quarter + " is the quarter you registered in.");
+            Console.WriteLine("Membership number: " + membershipNum);
+            Console.WriteLine("Total items: " + itemNum);
+            Console.WriteLine("Total cost before adjustment: " + totalCost);
+            Console.WriteLine("Final cost: " + finalCost);
+            Console.WriteLine("First discounted price: " + discountedTotalCost);
+            Console.WriteLine("Second discounted price: " + furtherDiscountedTotalCost);
+        }
+        /*if (5 < itemNum && itemNum < 10)
+        {
+            discountedTotalCost = totalCost - 20;
+        }
+        else (10 < itemNum)
+        {
+            discountedTotalCost = totalCost * 70 / 100;
+        }
+
+        if (discountedTotalCost = 0)
+        {
+            totalCost = totalCost * 0.13;
+        }
+
+        if (discountedTotalCost > 100)
+        {
+            furtherDiscountedTotalCost = discountedTotalCost * 97.5 / 100;
+        }
+        else (discountedTotalCost < 30)
+        {
+            furtherDiscountedTotalCost = discountedTotalCost - 2;
+        }
+
+        if (furtherDiscountedTotalCost < 0)
+        {
+            furtherDiscountedTotalCost = 0;
+        }*/
+
+        static double Discount(int a, double b) // discount by number of purchsed items, a = itemNum, b = totalCost
+        {
+            double discountedTotalCost;
+
+            if (5 < a && a < 10)
+            {
+                discountedTotalCost = b - 20;
+                return discountedTotalCost;
+            }
+            else if (10 < a)
+            {
+                discountedTotalCost = b * 70 / 100;
+                return discountedTotalCost;
+            }
+            else
+            {
+                return b;
+
+            }
+        }
+
+
+        static double Discount(double a)
         {
             double furtherDiscountedTotalCost;
 
