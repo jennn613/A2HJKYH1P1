2d6cada1fca1ffbe1954b32ee919c2c3ad3216ba
Hyunjin Kim <69027218+jennn613@users.noreply.github.com>
2021-10-03 13:36:08 +0900

    fix switch statement

--- A2HJKYH1p1/A2HJKYH1p1/Program.cs
+++ A2HJKYH1p1/A2HJKYH1p1/Program.cs
@@ -1,4 +1,4 @@
-﻿/*using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,125 +11,16 @@ namespace A2HJKYHS1P1
         static void Main(string[] args)
         {
             // Defining variables
-            string quarter; // yearly quarter that the customer registered
+            string quarter; // yearly quarter 
             string membershipNum; // membership number
             int itemNum; // number of items purchased
-            double itemCost; // cost of a single item
-            double totalCost; // total cost
-            double discountedTotalCost; // discounted total cost
-            double furtherDiscountedTotalCost; // total cost after discounting twice
-            double tax; // calculate tax
+            double itemCost; // cost of single item
+            double totalCost; // total cost of items
+            double discountedTotalCost; // 
+            double furtherDiscountedTotalCost; // 
+            double finalDiscountedCost;
+            double tax; // tax
             double finalCost; // final cost
-
-            /* --------------------------------------------------------------------------- */
-
-            // Getting user input
-            /*Console.WriteLine("Please enter the quarter you registered in: ");
-            quarter = Console.ReadLine();
-
-            Console.WriteLine("Please enter your membership number: ");
-            membershipNum = Console.ReadLine();
-
-            Console.WriteLine("Please enter the number of items you are purchasing today: ");
-            itemNum = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Please enter the cost of the item you are purchasing today: ");
-            itemCost = Convert.ToDouble(Console.ReadLine());
-
-            // Calculations
-            totalCost = itemCost * itemNum;
-            discountedTotalCost = Discount(itemNum, totalCost); // total cost after discounted
-            tax = 0.13 * totalCost;
-            finalCost = totalCost + tax;
-
-
-            if (discountedTotalCost < 0) 
-            {
-                discountedTotalCost = -(totalCost * 0.13);
-            }
-
-            //Console.WriteLine(discountedTotalCost);
-
-            furtherDiscountedTotalCost = Discount(discountedTotalCost);
-
-            if (furtherDiscountedTotalCost < 0)
-            {
-                furtherDiscountedTotalCost = 0;
-            }
-
-            //Console.WriteLine(furtherDiscountedTotalCost);
-
-            // Displaying information to the user
-            //Console.WriteLine("");
-            //Console.WriteLine(quarter + " is the quarter you registered in.");
-            //Console.WriteLine("Membership number: " + membershipNum);
-            //Console.WriteLine("Total items: " + itemNum);
-            //Console.WriteLine("Total cost before adjustment: " + totalCost);
-            //Console.WriteLine("Final cost: " + finalCost);
-
-
-        }
-
-        static double Discount(int a, double b) // discount by number of purchsed items, a = itemNum, b = totalCost
-        {
-            double discountedTotalCost;
-
-            if (5 < a && a < 10)
-            {
-                discountedTotalCost = b - 20;
-                return discountedTotalCost;
-            }
-            else if (10 < a)
-            {
-                discountedTotalCost = b * 70 / 100;
-                return discountedTotalCost;
-            }
-            else
-            {
-                return b;
-            }
-        }
-
-        static double Discount(double a) // discount by first discounted cost, a = itemNum, b = totalCost
-        {
-            double furtherDiscountedTotalCost;
-
-            if (a > 100)
-            {
-                furtherDiscountedTotalCost = a * 97.5 / 100;
-                return furtherDiscountedTotalCost;
-            }
-            else if (a < 30)
-            {
-                furtherDiscountedTotalCost = a - 2;
-                return furtherDiscountedTotalCost;
-            }
-            else
-            {
-                return a;
-            }
-        }
-    }
-}
-*/
-using System;
-
-namespace A2HJKYHS1P1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // Defining variables
-            string quarter;
-            string membershipNum;
-            int itemNum;
-            double itemCost;
-            double totalCost;
-            double discountedTotalCost;
-            double furtherDiscountedTotalCost;
-            double tax;
-            double finalCost;
 
             // Getting user input
             Console.WriteLine("Please enter the quarter you registered in: ");
@@ -152,7 +43,7 @@ namespace A2HJKYHS1P1
 
             if (discountedTotalCost < 0)
             {
-                discountedTotalCost = -(totalCost * 0.13);
+                discountedTotalCost = totalCost * 0.13;
             }
 
             furtherDiscountedTotalCost = Discount(discountedTotalCost);
@@ -165,56 +56,53 @@ namespace A2HJKYHS1P1
             switch (Convert.ToInt32(quarter))
             {
                 case 1:
-                    furtherDiscountedTotalCost - 15;
+                    finalDiscountedCost = furtherDiscountedTotalCost - 15;
+                    if (finalDiscountedCost < 0)
+                    {
+                        finalDiscountedCost = 0;
+                    }
                     break;
                 case 2:
-                    furtherDiscountedTotalCost - 15;
+                    finalDiscountedCost = furtherDiscountedTotalCost - 15;
+                    if (finalDiscountedCost < 0)
+                    {
+                        finalDiscountedCost = 0;
+                    }
                     break;
                 case 3:
-                    furtherDiscountedTotalCost - 5;
+                    finalDiscountedCost = furtherDiscountedTotalCost - 5;
+                    if (finalDiscountedCost < 0)
+                    {
+                        finalDiscountedCost = 0;
+                    }
                     break;
                 case 4:
-                    furtherDiscountedTotalCost - 1;
+                    finalDiscountedCost = furtherDiscountedTotalCost - 1;
+                    if (finalDiscountedCost < 0)
+                    {
+                        finalDiscountedCost = 0;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid yearly quarter. enter again");
+                    //quarter =  Console.ReadLine();
+                    //Console.WriteLine(quarter);
+                    //I want to change the value of quarter that they entered and make this switch statement work again. But I can't figure this out ;_;
                     break;
             }
 
+
+
             // Displaying information to the user
-            Console.WriteLine("");
-            Console.WriteLine(quarter + " is the quarter you registered in.");
-            Console.WriteLine("Membership number: " + membershipNum);
-            Console.WriteLine("Total items: " + itemNum);
-            Console.WriteLine("Total cost before adjustment: " + totalCost);
-            Console.WriteLine("Final cost: " + finalCost);
-            Console.WriteLine("First discounted price: " + discountedTotalCost);
-            Console.WriteLine("Second discounted price: " + furtherDiscountedTotalCost);
-        }
-        /*if (5 < itemNum && itemNum < 10)
-        {
-            discountedTotalCost = totalCost - 20;
-        }
-        else (10 < itemNum)
-        {
-            discountedTotalCost = totalCost * 70 / 100;
+            //Console.WriteLine(quarter + " is the quarter you registered in.");
+            //Console.WriteLine("Membership number: " + membershipNum);
+            //Console.WriteLine("Total items: " + itemNum);
+            //Console.WriteLine("Total cost before adjustment: " + totalCost);
+            //Console.WriteLine("Final cost: " + finalCost);
+            //Console.WriteLine("First discounted price: " + discountedTotalCost);
+            //Console.WriteLine("Second discounted price: " + furtherDiscountedTotalCost);
         }
 
-        if (discountedTotalCost = 0)
-        {
-            totalCost = totalCost * 0.13;
-        }
-
-        if (discountedTotalCost > 100)
-        {
-            furtherDiscountedTotalCost = discountedTotalCost * 97.5 / 100;
-        }
-        else (discountedTotalCost < 30)
-        {
-            furtherDiscountedTotalCost = discountedTotalCost - 2;
-        }
-
-        if (furtherDiscountedTotalCost < 0)
-        {
-            furtherDiscountedTotalCost = 0;
-        }*/
 
         static double Discount(int a, double b) // discount by number of purchsed items, a = itemNum, b = totalCost
         {
@@ -237,8 +125,7 @@ namespace A2HJKYHS1P1
             }
         }
 
-
-        static double Discount(double a)
+        static double Discount(double a) // discount by total cost, a = furtherDiscountedTotalCost;
         {
             double furtherDiscountedTotalCost;
 
@@ -257,5 +144,6 @@ namespace A2HJKYHS1P1
                 return a;
             }
         }
+
     }
 }
